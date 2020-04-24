1a4c6eee8c47bf06d3382af8696f581fafc39948
jerp86 <jerp4@hotmail.com>
2020-04-24 20:43:12 -0300

    Foco e preenchimento

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,16 +1,29 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
-export const Container = styled.View`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.View<ContainerProps>`
   width: 100%;
   height: 60px;
   padding: 0 16px;
   background: #232129;
   border-radius: 10px;
   margin-bottom: 8px;
+  border-width: 2px;
+  border-color: #232129;
 
   flex-direction: row;
   align-items: center;
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      border-color: #f79000;
+    `}
 `;
 
 export const TextInput = styled.TextInput`
