2acf351ac475a949c456a746b51f7da4283a2ada
jerp86 <jerp4@hotmail.com>
2020-04-23 23:52:15 -0300

    Importando fontes externas

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,3 +1,14 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: #f4ede8;
+  font-size: 24px;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px;
+`;
