e21c99671cb46b225956eaad270653f34a4443c8
jerp86 <jerp4@hotmail.com>
2020-07-13 22:57:55 -0300

    Criando estrutura da tela

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  return <Container />;
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Terça, dia 14 de julho de 2020 às 11:00h</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>OK</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
--- src/pages/AppointmentCreated/styles.ts
+++ src/pages/AppointmentCreated/styles.ts
@@ -1,8 +1,40 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  padding: 10px 20px;
+  padding: 0 24px;
+`;
+
+export const Title = styled.Text`
+  color: #f4ede8;
+  font-size: 32px;
+  font-family: 'RobotoSlab-Medium';
+  margin-top: 48px;
+  text-align: center;
+`;
+
+export const Description = styled.Text`
+  color: #999591;
+  font-family: 'RobotoSlab-Regular';
+  font-size: 18px;
+  margin-top: 16px;
+  text-align: center;
+`;
+
+export const OkButton = styled(RectButton)`
+  background: #ff9000;
+  padding: 12px 24px;
+  margin-top: 24px;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const OkButtonText = styled.Text`
+  color: #312e38;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 18px;
 `;
